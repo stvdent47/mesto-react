7d732b579e6685b93173a3bf732973d98a12c7f1
Anton Rodionov <stvdent47@gmail.com>
2020-10-07 02:28:41 +0300

    cards rendering&&user info getting&&setting

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Card from './Card.jsx';
 import profilePhoto from '../images/profile-photo.jpg';
+import api from '../utils/Api.js';
 
 const Main = (props) => {
+  const [userName, setUserName] = useState('...');
+  const [userDescription, setUserDescription] = useState('...');
+  const [userAvatar, setUserAvatar] = useState(profilePhoto);
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    api
+      .getProfileInfo()
+      .then((res) => {
+        setUserName(res.name);
+        setUserDescription(res.about);
+        setUserAvatar(res.avatar);
+      })
+      .catch((err) => alert(err));
+
+    api
+      .getCards()
+      .then((res) => setCards(res))
+      .catch((err) => alert(err));
+  }, []);
+
   return (
     <main className='main'>
       <section className='profile'>
-        <div
-          className='profile__photo-container'
-          onClick={props.onEditAvatar}
-        >
-          <img
-            src={profilePhoto}
-            alt='фото профиля'
-            className='profile__photo'
-          />
+        <div className='profile__photo-container' onClick={props.onEditAvatar}>
+          <img src={userAvatar} alt='фото профиля' className='profile__photo' />
         </div>
 
         <div className='profile__info'>
           <div className='profile__title'>
-            <h1 className='profile__name'>...</h1>
+            <h1 className='profile__name'>{userName}</h1>
             <button
               className='profile__edit-button'
               type='button'
@@ -27,7 +43,7 @@ const Main = (props) => {
             ></button>
           </div>
 
-          <p className='profile__description'>...</p>
+          <p className='profile__description'>{userDescription}</p>
         </div>
 
         <button
@@ -39,10 +55,21 @@ const Main = (props) => {
       </section>
 
       <section className='photo-elements'>
-        <ul className='photo-elements__list'></ul>
+        <ul className='photo-elements__list'>
+          {cards.map((item) => {
+            return (
+              <Card
+                key={item._id}
+                link={item.link}
+                name={item.name}
+                likes={item.likes.length}
+              />
+            );
+          })}
+        </ul>
       </section>
     </main>
   );
-}
+};
 
 export default Main;
