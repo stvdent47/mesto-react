ee1040fab45d701c4b40d95b85d1baeccbac1308
Anton Rodionov <stvdent47@gmail.com>
2020-10-07 21:19:48 +0300

    Updates

---
+++ precache-manifest.82b2479bae9f4134bc5193539c54c1cb.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7d77c3c16442542a41be4e88c6bdfe",
+    "url": "/mesto-react/index.html"
+  },
+  {
+    "revision": "364a80455eee7450a337",
+    "url": "/mesto-react/static/css/main.97a1698d.chunk.css"
+  },
+  {
+    "revision": "64b9e99119d99a59aa54",
+    "url": "/mesto-react/static/js/2.458dbe58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mesto-react/static/js/2.458dbe58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "364a80455eee7450a337",
+    "url": "/mesto-react/static/js/main.8582513e.chunk.js"
+  },
+  {
+    "revision": "6e04319a44e5e6859875",
+    "url": "/mesto-react/static/js/runtime-main.36fb0e45.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/mesto-react/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/mesto-react/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "07e69b53f8cf91da94096d1abf484302",
+    "url": "/mesto-react/static/media/Inter-BlackItalic.07e69b53.woff"
+  },
+  {
+    "revision": "daa1ca3cebd42cf5ebd53d9a6942b2cf",
+    "url": "/mesto-react/static/media/Inter-BlackItalic.daa1ca3c.woff2"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "/mesto-react/static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "aed27700d84e327fda56b4a427b03061",
+    "url": "/mesto-react/static/media/Inter-Bold.aed27700.woff2"
+  },
+  {
+    "revision": "8ef77a031c5b96e53f2eeeb009232181",
+    "url": "/mesto-react/static/media/Inter-BoldItalic.8ef77a03.woff2"
+  },
+  {
+    "revision": "e0879d64763e509f309940120e7d1f5a",
+    "url": "/mesto-react/static/media/Inter-BoldItalic.e0879d64.woff"
+  },
+  {
+    "revision": "38bc51bccb7dd07b2752c11888cbdc89",
+    "url": "/mesto-react/static/media/Inter-ExtraBold.38bc51bc.woff"
+  },
+  {
+    "revision": "92d16aee8fb5f5c5cfd660b2d07e1148",
+    "url": "/mesto-react/static/media/Inter-ExtraBold.92d16aee.woff2"
+  },
+  {
+    "revision": "0e4b21ebad8d568078450713ee9339cf",
+    "url": "/mesto-react/static/media/Inter-ExtraBoldItalic.0e4b21eb.woff"
+  },
+  {
+    "revision": "57ea76d011f6fff8cb7013b783bad58e",
+    "url": "/mesto-react/static/media/Inter-ExtraBoldItalic.57ea76d0.woff2"
+  },
+  {
+    "revision": "4bd040df09ea4765148b5743b8493e5f",
+    "url": "/mesto-react/static/media/Inter-ExtraLight.4bd040df.woff"
+  },
+  {
+    "revision": "4d9f96f8504cd034ef7e5f25434ca550",
+    "url": "/mesto-react/static/media/Inter-ExtraLight.4d9f96f8.woff2"
+  },
+  {
+    "revision": "54d3d9a59ea904ebd11c3ac308893bfb",
+    "url": "/mesto-react/static/media/Inter-ExtraLightItalic.54d3d9a5.woff2"
+  },
+  {
+    "revision": "84c26656e02913a54131fc64431212ae",
+    "url": "/mesto-react/static/media/Inter-ExtraLightItalic.84c26656.woff"
+  },
+  {
+    "revision": "9528384cf7aebfa81a198626cb05f5e8",
+    "url": "/mesto-react/static/media/Inter-Italic.9528384c.woff2"
+  },
+  {
+    "revision": "e4ad3666e223c64e4df963cec740bf09",
+    "url": "/mesto-react/static/media/Inter-Italic.e4ad3666.woff"
+  },
+  {
+    "revision": "5baca21acf845c8e746f08675f40300b",
+    "url": "/mesto-react/static/media/Inter-Light.5baca21a.woff2"
+  },
+  {
+    "revision": "b9920de0749addeb5a53462fb5bbe070",
+    "url": "/mesto-react/static/media/Inter-Light.b9920de0.woff"
+  },
+  {
+    "revision": "0555a46cda7fc3406a875d6bacf8fcff",
+    "url": "/mesto-react/static/media/Inter-LightItalic.0555a46c.woff"
+  },
+  {
+    "revision": "adc70179a9e60e14f2a4f7d106166004",
+    "url": "/mesto-react/static/media/Inter-LightItalic.adc70179.woff2"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/mesto-react/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/mesto-react/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "417907d2be56d39b2a9c0b52fc8a00e8",
+    "url": "/mesto-react/static/media/Inter-MediumItalic.417907d2.woff"
+  },
+  {
+    "revision": "565a7104c497ccb1bf12832d8b341861",
+    "url": "/mesto-react/static/media/Inter-MediumItalic.565a7104.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/mesto-react/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/mesto-react/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "1db6c55ca0f2a97fbce3b0b3903a7bb7",
+    "url": "/mesto-react/static/media/Inter-SemiBold.1db6c55c.woff"
+  },
+  {
+    "revision": "dd8a55ef7058cdaeb96ef9fc65344726",
+    "url": "/mesto-react/static/media/Inter-SemiBold.dd8a55ef.woff2"
+  },
+  {
+    "revision": "81678d1a50a895cc1232f3e287976083",
+    "url": "/mesto-react/static/media/Inter-SemiBoldItalic.81678d1a.woff"
+  },
+  {
+    "revision": "ac201e30486307c75c5038f61df9fd9f",
+    "url": "/mesto-react/static/media/Inter-SemiBoldItalic.ac201e30.woff2"
+  },
+  {
+    "revision": "850febbe5e8a7625c5603061d2e7c0ce",
+    "url": "/mesto-react/static/media/Inter-Thin.850febbe.woff2"
+  },
+  {
+    "revision": "ead42837911f0b55b0877c12b50d9157",
+    "url": "/mesto-react/static/media/Inter-Thin.ead42837.woff"
+  },
+  {
+    "revision": "a76db06556290ed3bd198a72b24d8006",
+    "url": "/mesto-react/static/media/Inter-ThinItalic.a76db065.woff"
+  },
+  {
+    "revision": "e08d9b2a5079b81cc39804c2a7406254",
+    "url": "/mesto-react/static/media/Inter-ThinItalic.e08d9b2a.woff2"
+  },
+  {
+    "revision": "2690e3c25733fb852b55b3f8d651a431",
+    "url": "/mesto-react/static/media/Inter-italic.var.2690e3c2.woff2"
+  },
+  {
+    "revision": "90e8f61d26f65b5ff0acc45ddf6740ea",
+    "url": "/mesto-react/static/media/Inter-roman.var.90e8f61d.woff2"
+  },
+  {
+    "revision": "4b976905f2be1cb9201f5d94afd9edd2",
+    "url": "/mesto-react/static/media/Inter.var.4b976905.woff2"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/mesto-react/static/media/add-button-background-image.b2fce4ac.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/mesto-react/static/media/avatar-edit-image.d476a8b8.svg"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "/mesto-react/static/media/close-button-background-image.5df32a06.svg"
+  },
+  {
+    "revision": "d80783439d382b46ce24b8da369767d4",
+    "url": "/mesto-react/static/media/delete-button-background-image.d8078343.svg"
+  },
+  {
+    "revision": "ac9f293799cc1ea88155b5abd267b418",
+    "url": "/mesto-react/static/media/edit-button-background-image.ac9f2937.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/mesto-react/static/media/like-button-active.5bbb05f7.svg"
+  },
+  {
+    "revision": "30f5099ae60f46326fd48cc86c724b5f",
+    "url": "/mesto-react/static/media/like-button-image.30f5099a.svg"
+  },
+  {
+    "revision": "c6f110191388042ab958ac5e9b285425",
+    "url": "/mesto-react/static/media/logo.c6f11019.svg"
+  },
+  {
+    "revision": "4a1ce1f1a6e526a15c9a42459d94ded7",
+    "url": "/mesto-react/static/media/profile-photo.4a1ce1f1.jpg"
+  }
+]);
